4b83269a1585b4bb7d73149a28ecaeeddd84233c
jspc <james@zero-internet.org.uk>
2019-11-28 17:01:05 +0900

    Enhance docs

--- article_classifier/classify.py
+++ article_classifier/classify.py
@@ -4,14 +4,31 @@ import spacy
 import en_core_web_sm
 
 class Classify():
+    '''
+    Classify takes a set of text and extracts data useful for NLP tasks.
+
+    It is largely designed to work with news articles about companies, and can
+    be used to normalise text for machine learning problems.
+    '''
     nlp = en_core_web_sm.load()
 
     def __init__(self, article_body, orgs_dict={}):
+        '''
+        Create a new instance of a Classified article
+
+        Keyword arguments:
+        article_body -- A string representation of the text within an article
+        orgs_dict -- A dict containing a mapping of person names to the organisation they belong to
+        '''
         self.doc = self.nlp(article_body)
         self.__orgs = orgs_dict
 
 
     def entities(self):
+        '''
+        Return the named organisation entities from the article, including the
+        organisations named persons belong to
+        '''
         entities = []
         unknown = []
 
@@ -30,6 +47,15 @@ class Classify():
 
 
     def normalise(self):
+        '''
+        Return the article body after the following transformations:
+
+        1 -- Removal of any recognised entity
+        2 -- Removal of stop words
+        3 -- Lematisation of words
+
+        This string can be used in ML models
+        '''
         non_entities = self.__non_entities(self.doc)
         non_stops = self.__de_stopword(non_entities)
         lematised = self.__lematise(non_stops)
